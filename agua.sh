158fa32b659fc79d4892dfd570b102ece581fd8f
Jorge Lucas V Jabczenski <40120369+JorgeJabczenski@users.noreply.github.com>
2020-09-25 09:31:55 -0300

    Create agua.sh

---
+++ agua.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Baixa os dados
+wget -q http://site.sanepar.com.br/grupos-rodizio
+
+# Separa bonitinho
+cat grupos-rodizio | grep 'Grupo 3 SAIC' -m2 -B4 | grep 'date-display' | cut -d'"' -f5 | sed 's/[<>]//g' | sed 's/\/span//g' | cut -d'p' -f1 | sed 's/.$//g' > .dias
+
+i=0
+
+while IFS= read -r line; do
+    if [ $((i%2)) == 0 ]
+    then
+        echo "Começa : $line";
+    else
+        echo "Acaba  : $line";
+    fi
+    i=($i+1)
+done < .dias
